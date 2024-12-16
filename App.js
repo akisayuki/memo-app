2622f18bfd67301759f7332d8fbb484e5de0d734
akisayuki <191073927+akisayuki@users.noreply.github.com>
2024-12-16 14:02:29 +0900

    [add]Navigationの設定
    react-navigationを利用してメモ一覧画面を設定した。

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './src/MainScreen';
+
+//Navigationの設定
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: {
+      screen: MainScreen,
+      options: {
+        title: 'メモ一覧'
+      }
+    }
+  }
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
   return (
-    <MainScreen />
+    <Navigation />
   );
 }
