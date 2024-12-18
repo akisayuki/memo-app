39b08a26b194a817631ded19792a575e740096f2
akisayuki <191073927+akisayuki@users.noreply.github.com>
2024-12-18 10:47:47 +0900

    [add]仮データを表示
    FlatListでデータを画面に表示させた。

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,21 +1,76 @@
 //メモ一覧（ホーム）画面
 
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+//仮実装のデータ
+const data = [
+    {
+        title: "好きな食べ物",
+        contents: "ネギ",
+        //TODO タイムスタンプを取得
+        id: 0
+    },
+    {
+        title: "明日の気温",
+        contents: "午前中は寒い。昼間は20度まで上がり、夜はまた冷える。",
+        id: 1
+    },
+    {
+        title: "今週の夕飯",
+        contents: "カレー2回、親子丼、中華丼、冷凍パスタ",
+        id: 2
+    }
+];
+
+//配列の各要素を描画
+const Item = ({item}) => {
+    return (
+        <View style={styles.itemContainer}>
+            <Text style={styles.itemTitle}>{item.title}</Text>
+            <Text style={styles.itemText}>{item.contents}</Text>
+        </View>
+    );
+}
+
+//メイン画面
 const MainScreen = () => {
     return (
-        <View style={styles.container}>
-            <Text>メモ一覧画面</Text>
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={data}
+                    renderItem={({item}) => {
+                        return (
+                            //Itemコンポーネントの引数を設定
+                            <Item item={item}/>
+                        );
+                    }}
+                    keyExtractor={item => item.id}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        margin: '5%',
         justifyContent: 'center'
+    },
+    itemContainer: {
+        marginTop: 16,
+        paddingBottom: 12,
+        borderBottomWidth: 1
+    },
+    itemTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    itemText: {
+        fontSize: 16,
+        marginTop: 12
     }
 });
 
