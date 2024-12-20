81235280af94a75217f9ccef850baee3e7dd39d0
akisayuki <191073927+akisayuki@users.noreply.github.com>
2024-12-20 13:37:47 +0900

    [add]入力フィールドの作成
    タイトルと本文を入力可能にし、スクロールができるようにした。

--- src/NewEntryScreen.js
+++ src/NewEntryScreen.js
@@ -1,7 +1,42 @@
 //新規作成画面
 
-const NewEntryScreen = () => {
+import { Input, ListItem } from "@rneui/themed";
+import { ScrollView, StyleSheet, Text, TextInput } from "react-native"
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+const NewEntryScreen = () => {
+    return(
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <ScrollView keyboardDismissMode="on-drag">
+                    <TextInput
+                        multiline
+                        placeholder="タイトル"
+                        style={styles.textBox}
+                    />
+                    <TextInput
+                        multiline
+                        placeholder="本文"
+                        style={styles.textBox}
+                    />
+                </ScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+    textBox: {
+        marginLeft: 10,
+        marginTop: 10,
+        marginBottom: 10,
+        borderBottomColor: 'silver',
+        borderBottomWidth: 1,
+        fontSize: 16
+    }
+});
 
 export default NewEntryScreen
