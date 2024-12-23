4499846c84c758ff3dfe93d6d4ed51ad24f23f8f
akisayuki <191073927+akisayuki@users.noreply.github.com>
2024-12-23 15:22:28 +0900

    [update]入力欄がキーボードに隠れないように修正
    KeyboardAvoidingViewのbehaviorをpositionに設定し、keyboardVerticalOffsetを大きな値（ここでは100）に設定した。

--- src/NewEntryScreen.js
+++ src/NewEntryScreen.js
@@ -1,25 +1,46 @@
 //新規作成画面
 
 import { Input, ListItem } from "@rneui/themed";
-import { ScrollView, StyleSheet, Text, TextInput } from "react-native"
+import { useState } from "react";
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const NewEntryScreen = () => {
     return(
         <SafeAreaProvider>
             <SafeAreaView>
-                <ScrollView keyboardDismissMode="on-drag">
-                    <TextInput
-                        multiline
-                        placeholder="タイトル"
-                        style={styles.textBox}
-                    />
-                    <TextInput
-                        multiline
-                        placeholder="本文"
-                        style={styles.textBox}
-                    />
-                </ScrollView>
+                {/*欄外をタッチするとキーボードを隠す*/}
+                <TouchableWithoutFeedback
+                    onPress={() => {
+                        Keyboard.dismiss()
+                    }}
+                >
+                    <ScrollView 
+                        keyboardDismissMode="on-drag"
+                    >
+                        <KeyboardAvoidingView
+                            style={styles.container}
+                            behavior={"position"}
+                            keyboardVerticalOffset={100}
+                        >
+                            <TextInput
+                                multiline
+                                placeholder="タイトル"
+                                style={styles.textLine}
+                            />
+                            <TextInput
+                                multiline
+                                placeholder="本文"
+                                style={styles.textBox}
+                            />
+                            <TextInput
+                                multiline
+                                placeholder="URL"
+                                style={styles.textLine}
+                            />
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+                </TouchableWithoutFeedback>
             </SafeAreaView>
         </SafeAreaProvider>
     );
@@ -27,15 +48,21 @@ const NewEntryScreen = () => {
 
 const styles = StyleSheet.create({
     container: {
-        
+        flex: 1,
     },
-    textBox: {
-        marginLeft: 10,
-        marginTop: 10,
-        marginBottom: 10,
+    textLine: {
+        margin: 10,
         borderBottomColor: 'silver',
         borderBottomWidth: 1,
-        fontSize: 16
+        fontSize: 16,
+        maxHeight: 80
+    },
+    textBox: {
+        margin: 10,
+        borderColor: 'silver',
+        borderWidth: 1,
+        fontSize: 16,
+        maxHeight: 300
     }
 });
 
