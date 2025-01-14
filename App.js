fe272c0672293bdc24dcd91d44628529a37842c7
akisayuki <191073927+akisayuki@users.noreply.github.com>
2025-01-14 17:03:23 +0900

    [fix]データベースの初期化ができない問題を修正
    テーブル名に予約後が採用されていた点を修正し、useEffect内でinitDatabaseを宣言し直すことで、アプリがマウントされた際に一度だけ初期化されるよう修正した。

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './src/MainScreen';
 import NewEntryScreen from './src/NewEntryScreen';
 import { useEffect } from 'react';
-import { initDatabase } from './src/components/CRUDoperations';
+import { initDatabase } from './src/components/DatabaseOperations';
 
 //Navigationの設定
 const MyStack = createNativeStackNavigator({
@@ -31,9 +31,18 @@ const Navigation = createStaticNavigation(MyStack);
 export default function App() {
   //データベースの初期化をアプリ起動時に実行
   useEffect(() => {
-    initDatabase()
-  }, [])
-  
+    //useEffectで非同期関数を使う場合は、ラップ関数を定義して呼び出す必要がある
+    const init = async () => {
+      try {
+        await initDatabase();
+        console.log('Database initialized successfully.');
+      } catch (error) {
+        console.error('Error initializing database:', error);
+      }
+    }
+    init();
+  }, []);
+
   return (
     <Navigation />
   );
--- src/components/DatabaseOperations.js
+++ src/components/DatabaseOperations.js
@@ -3,34 +3,35 @@
 import * as SQLite from 'expo-sqlite'
 
 
-const db = await SQLite.openDatabaseAsync('app.db');
-
 //データベースの初期化
 export const initDatabase = async () => {
     try {
-        //entriesテーブルとreferencesテーブルを作成
+        const db = await SQLite.openDatabaseAsync('app.db');
+        //PRAGMAの設定
         await db.execAsync(`
             PRAGMA journal_mode = WAL;
             PRAGMA foreign_keys = ON;
-
+        `);
+        //entriesテーブルとreference_listテーブルを作成
+        await db.execAsync(`
             CREATE TABLE IF NOT EXISTS entries(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT,
                 body TEXT
             );
-
-            CREATE TABLE IF NOT EXISTS references(
+        
+            CREATE TABLE IF NOT EXISTS reference_list(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 entry_id INTEGER,
                 reference TEXT,
                 FOREIGN KEY(entry_id) REFERENCES entries(id)
             );
-        `)
+        `);
     } catch (error) {
         console.error('Error initializing database:', error);
     }
 }
-
+/*
 //データの保存
 export const onSaveData = async (title, body, references) => {
     try {
@@ -55,3 +56,31 @@ export const onSaveData = async (title, body, references) => {
         console.error('Error saving entries and references:', error);
     }
 }
+
+//データを取得する
+export const getAllData = async () => {
+    try {
+        const entriesResult = await db.getAllAsync('SELECT * FROM entries;');
+        const referencesResult = await db.getAllAsync('SELECT * FROM references;');
+
+        //entriesにreferencesを紐づける(マッピング)
+        const referencesMap = {};
+        referencesResult.forEach((reference) => {
+            //referencesMap[entry_id]が存在しない場合、空の配列を追加して初期化
+            if (!referencesMap[reference.entry_id]) {
+                referencesMap[reference.entry_id] = [];
+            }
+            referencesMap[reference.entry_id].push(reference.reference);
+        });
+
+        //各エントリーに対応する参考文献を追加する
+        entriesResult.forEach((entry) => {
+            //対応する値がない場合は、空の配列を追加する
+            return entry.references = referencesMap[entry.id] || [];
+        });
+
+        return entriesResult;
+    } catch (error) {
+        console.error('Error retrieving entries and references:', error);
+    }
+}*/
