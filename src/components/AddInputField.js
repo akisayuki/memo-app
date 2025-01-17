e6cf66f962b66094bbe20d241348f6470f946830
akisayuki <191073927+akisayuki@users.noreply.github.com>
2025-01-17 19:08:08 +0900

    [add]保存ボタンを押すと自動的にメイン画面に遷移するように設定
    handleSaveイベントハンドラを実装し、保存を待ってから画面遷移を実行するようにした。

--- src/components/AddInputField.js
+++ src/components/AddInputField.js
@@ -6,6 +6,7 @@ import ReferenceFieldList from "./ReferenceFieldList";
 import { StyleSheet, View } from "react-native";
 import { Button } from "@rneui/themed";
 import { onSaveData } from "./DatabaseOperations";
+import { useNavigation } from "@react-navigation/native";
 
 const AddInputField = () => {
     const [title, setTitle] = useState('');
@@ -18,6 +19,19 @@ const AddInputField = () => {
             style: styles.inputLine
         }
     ]);
+
+    const navigation = useNavigation();
+
+    //保存と画面遷移を非同期で行うイベントハンドラ
+    const handleSave = async () => {
+        try {
+            //保存を行ってから画面遷移を実行
+            await onSaveData(title, body, reference);
+            navigation.goBack();
+        } catch (error) {
+            console.error('Error saving or going back:', error);
+        }
+    }
 
     return(
         <View>
@@ -37,7 +51,7 @@ const AddInputField = () => {
                 iconContainerStyle={styles.iconContainer}
                 containerStyle={styles.saveButtonContainer}
                 buttonStyle={styles.saveButtonStyle}
-                onPress={() => onSaveData(title, body, reference)}
+                onPress={handleSave}
             />
         </View>
     );
