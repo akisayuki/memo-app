1ec4071a194c54dc9091358dad854a73537d3595
akisayuki <191073927+akisayuki@users.noreply.github.com>
2025-02-05 13:07:55 +0900

    [add]バリデーションを追加
    新規作成、編集時に未入力のタイトルと参考文献リストがある場合はアラートを出して保存処理を保留するようにした。

--- src/screens/EditEntryScreen.js
+++ src/screens/EditEntryScreen.js
@@ -3,6 +3,7 @@
 import { useNavigation } from "@react-navigation/native";
 import AddInputField from "../components/AddInputField";
 import { updateData } from "../components/DatabaseOperations";
+import { Alert } from "react-native";
 
 
 const EditEntryScreen = ({ route }) => {
@@ -12,13 +13,28 @@ const EditEntryScreen = ({ route }) => {
     //更新用のイベントハンドラ
     const handleUpdate = async (editData) => {
         try {
+            //タイトルが空の場合、アラートを表示
+            if (!editData.title.trim()) {
+                Alert.alert('タイトルを入力してください');
+                return;
+            }
+
+            //isDeletedフラグが立っている、かつ値が未入力なものを検出
+            const emptyReferenceFields = editData.reference
+                .some(ref => !ref.isDeleted && !ref.value.trim());
+
+            if (emptyReferenceFields) {
+                Alert.alert('未入力の参考文献フォームは削除してください');
+                return;
+            }
+
             const updateEntry = await updateData(
                 editData.id,
                 editData.title,
                 editData.body,
                 editData.reference
             );
-            
+
             navigation.setParams({ inputData: updateEntry });
             navigation.navigate('Home');
         } catch (error) {
--- src/screens/NewEntryScreen.js
+++ src/screens/NewEntryScreen.js
@@ -4,6 +4,7 @@ import React from "react";
 import AddInputField from "../components/AddInputField";
 import { onSaveData } from "../components/DatabaseOperations";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 const NewEntryScreen = () => {
     const initialData = {
@@ -16,14 +17,28 @@ const NewEntryScreen = () => {
 
     //新規保存を実行する関数
     const handleSave = async (formData) => {
-            try {
-                //保存を行ってから画面遷移を実行
-                await onSaveData(formData.title, formData.body, formData.reference);
-                navigation.goBack();
-            } catch (error) {
-                console.error('Error saving or going back:', error);
+        try {
+            //タイトルが未入力の場合にアラートを表示
+            if (!formData.title.trim()) {
+                Alert.alert('タイトルを入力してください');
+                return;
             }
+
+            //削除されていない参考文献リストで未入力なものを検出
+            const emptyReferenceFields = formData.reference
+                .some(ref => !ref.isDeleted && !ref.value.trim());
+            if (emptyReferenceFields) {
+                Alert.alert('未入力の参考文献フォームは削除してください');
+                return;
+            }
+
+            //保存を行ってから画面遷移を実行
+            await onSaveData(formData.title, formData.body, formData.reference);
+            navigation.goBack();
+        } catch (error) {
+            console.error('Error saving or going back:', error);
         }
+    }
 
     return (
         <AddInputField
